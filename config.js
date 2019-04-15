db0235b264fc02b9a65015975dade115ec90359c
rigwild <contact@asauvage.fr>
2019-04-15 10:31:37 +0200

    Log docker app to file system

--- config.js
+++ config.js
@@ -31,8 +31,8 @@ export const logger = winston.createLogger({
   level: 'info',
   format: winston.format.json(),
   transports: [
-    new winston.transports.File({ filename: 'combined.log' }),
-    new winston.transports.File({ filename: 'error.log', level: 'error' }),
+    new winston.transports.File({ filename: 'logs/combined.log' }),
+    new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
     new winston.transports.Console({
       level: 'debug',
       handleExceptions: true,
