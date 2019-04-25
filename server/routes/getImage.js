88b91170a2f5e484a4328f080451b400dc5df739
rigwild <contact@asauvage.fr>
2019-04-25 15:43:45 +0200

    Added apiDoc documentation

--- server/routes/getImage.js
+++ server/routes/getImage.js
@@ -10,6 +10,74 @@ import { asyncMiddleware, checkSceneName, checkRequiredParameters, getSceneFiles
 const router = express.Router()
 
 /**
+ * @api {get} /getImage?sceneName=:sceneName&imageQuality=:imageQuality Get an image from a scene
+ * @apiVersion 0.1.0
+ * @apiName GetImage
+ * @apiGroup API
+ *
+ * @apiDescription Get an image from a scene with the required quality
+ *
+ * @apiParam {String} sceneName The selected scene
+ * @apiParam {Number} imageQuality The required quality of the image
+ *
+ * @apiHeader (Response Headers) {String} Content-Type application/json; charset=utf-8
+ *
+ * @apiExample Usage example
+ * curl -i -L -X GET "http://localhost:5000/api/getImage?sceneName=bathroom&imageQuality=200"
+ *
+ * @apiSuccess {String} data Path to the image
+ * @apiSuccessExample {json} Success response example
+ * HTTP/1.1 200 OK /api/getImage?sceneName=bathroom&imageQuality=200
+ * {
+ *   "data": "/api/images/bathroom/bathroom_00200.png"
+ * }
+ *
+ * @apiError (Error 4xx) 400 Missing parameter(s)
+ * @apiErrorExample {json} Missing parameter
+ * HTTP/1.1 400 Bad Request
+ * {
+ *   "message": "Missing parameter(s). Required parameters : sceneName, imageQuality."
+ * }
+ *
+ * @apiError (Error 4xx) 400 Invalid query parameter
+ * @apiErrorExample {json} Invalid query parameter(s)
+ * HTTP/1.1 400 Bad Request
+ * {
+ *   "message": "Invalid query parameter(s).",
+ *   "data": [
+ *     "The requested scene name \".//../\" is not valid.",
+ *     "The specified quality is not an integer."
+ *   ]
+ * }
+ *
+ * @apiError (Error 4xx) 404 Quality not found
+ * @apiErrorExample {json} Quality not found
+ * HTTP/1.1 404 Not Found
+ * {
+ *   "message": "The requested quality (9999) was not found for the requested scene (bathroom)."
+ * }
+ *
+ * @apiError (Error 5xx) 500 Can't access the `IMAGES_PATH` directory
+ * @apiErrorExample {json} Images directory not accessible
+ * HTTP/1.1 500 Internal Server Error
+ * {
+ *   "message": "Can't access the \"images\" directory. Check it exists and you have read permission on it"
+ * }
+ *
+ * @apiError (Error 5xx) 500 Failed to parse a file's name
+ * @apiErrorExample {json} Failed to parse a file's name
+ * HTTP/1.1 500 Internal Server Error
+ * {
+ *   "message": "Failed to parse file names in the \"bathroom\"'s scene directory.",
+ *   "data": [
+ *     "The file name does not match convention (scene_000150.ext - /^(.*)?_([0-9]{2,})\\.(.*)$/) : \"bathroom_adz00020.png\".",
+ *     "The file name does not match convention (scene_000150.ext - /^(.*)?_([0-9]{2,})\\.(.*)$/) : \"bathroom_adz00020.png\"."
+ *   ]
+ * }
+ *
+ */
+
+/**
  * @typedef {Object} Image
  * @property {string} link the link (URL) to an image on the app
  * @property {string} path the path to the image in the file system
--- server/routes/listSceneQualities.js
+++ server/routes/listSceneQualities.js
@@ -5,6 +5,65 @@ import { asyncMiddleware, checkRequiredParameters, getSceneFilesData } from '../
 
 const router = express.Router()
 
+/**
+ * @api {get} /listSceneQualities?sceneName=:sceneName Get a list of available qualities for a scene
+ * @apiVersion 0.1.0
+ * @apiName ListScenesQualities
+ * @apiGroup API
+ *
+ * @apiDescription List all available qualities for a given scene
+ *
+ * @apiParam {String} sceneName The selected scene
+ *
+ * @apiHeader (Response Headers) {String} Content-Type application/json; charset=utf-8
+ *
+ * @apiExample Usage example
+ * curl -i -L -X GET "http://localhost:5000/api/listSceneQualities?sceneName=bathroom"
+ *
+ * @apiSuccess {Number[]} data List of available qualities
+ * @apiSuccessExample {json} Success response example
+ * HTTP/1.1 200 OK /api/listSceneQualities?sceneName=bathroom
+ * {
+ *   "data": [
+ *      10,
+ *      20,
+ *      30
+ *   ]
+ * }
+ *
+ * @apiError (Error 4xx) 400 Missing parameter(s)
+ * @apiErrorExample {json} Missing parameter
+ * HTTP/1.1 400 Bad Request
+ * {
+ *   "message": "Missing parameter(s). Required parameters : sceneName."
+ * }
+ *
+ * @apiError (Error 4xx) 400 The requested scene name is not valid
+ * @apiErrorExample {json} Invalid scene name
+ * HTTP/1.1 400 Bad Request
+ * {
+ *   "message": "The requested scene name \"bathroom/../\" is not valid."
+ * }
+ *
+ * @apiError (Error 5xx) 500 Can't access the `IMAGES_PATH` directory
+ * @apiErrorExample {json} Images directory not accessible
+ * HTTP/1.1 500 Internal Server Error
+ * {
+ *   "message": "Can't access the \"images\" directory. Check it exists and you have read permission on it"
+ * }
+ *
+ * @apiError (Error 5xx) 500 Failed to parse a file's name
+ * @apiErrorExample {json} Failed to parse a file's name
+ * HTTP/1.1 500 Internal Server Error
+ * {
+ *   "message": "Failed to parse file names in the \"bathroom\"'s scene directory.",
+ *   "data": [
+ *     "The file name does not match convention (scene_000150.ext - /^(.*)?_([0-9]{2,})\\.(.*)$/) : \"bathroom_adz00020.png\".",
+ *     "The file name does not match convention (scene_000150.ext - /^(.*)?_([0-9]{2,})\\.(.*)$/) : \"bathroom_adz00020.png\"."
+ *   ]
+ * }
+ */
+
 // Route which returns a list of all available qualities for a scene
 router.get('/', asyncMiddleware(async (req, res) => {
   // Check the request contains all the required parameters
